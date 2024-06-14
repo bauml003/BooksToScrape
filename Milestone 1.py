1af5f75d6e04871090acef9763df3c476df29979
Cherie Bluhm <bauml003@gmail.com>
2024-06-14 00:11:36 -0500

    Initial commit

---
+++ Milestone 1.py
@@ -0,0 +1,14 @@
+# Pick any single product page (i.e., a single book) on Books to Scrape, and write a
+# Python script that visits this page and extracts the following information:
+# ● product_page_url
+# ● universal_ product_code (upc)
+# ● book_title
+# ● price_including_tax
+# ● price_excluding_tax
+# ● quantity_available
+# ● product_description
+# ● category
+# ● review_rating
+# ● image_url
+# Write the data to a CSV file using the above fields as column headings.
+
