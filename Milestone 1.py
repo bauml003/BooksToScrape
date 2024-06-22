b93a99f2c45a930ea089194b7df23a68661eaa83
Cherie Bluhm <bauml003@gmail.com>
2024-06-22 16:39:35 -0500

    SUCCESS: Extract Star Rating

--- Milestone 1.py
+++ Milestone 1.py
@@ -8,14 +8,14 @@
 # DONE ● quantity_available
 # DONE ● product_description
 # DONE ● category
-#  ● review_rating
+# DONE ● review_rating
 # DONE ● image_url
+# ● category
 # Write the data to a CSV file using the above fields as column headings.
 
 from bs4 import BeautifulSoup
 import requests
 import csv
-# import pandas as pd
 
 # Create a dictionary to hold the data
 table_data = {}
@@ -39,45 +39,50 @@ for book in books:
     # Go to the book page and scrape the data
     book_soup = BeautifulSoup(requests.get(book_url_full).text, "html.parser")
 
-    # # find the table of data on the book page
-    # Information_Table = book_soup.find("table")
-    #
-    # # Extract the data from the book page's table
-    # n = 0
-    # for label_itm in Information_Table.findAll("th"):
-    #     value_item = Information_Table.select('td')[n].text
-    #     table_data[label_itm.text] = value_item
-    #     n += 1
-    #
-    # # Extract the book title
-    # book_title = book_soup.find("h1").text
-    # table_data['book_title'] = book_title
-    #
-    # # Extract the image URL
-    # image_url = book_soup.find("img")["src"]
-    # table_data['image_url'] = image_url
-    #
-    # # Extract the product description (assumption: descriptions will be more _
-    # # than 60 characters; other targets not usable because aren't as long)
-    # for product_description in book_soup.find_all("p"):
-    #     if len(product_description.text) > 60:
-    #         product_description = product_description.text
-    #         table_data['product_description'] = product_description
+    # find the table of data on the book page
+    Information_Table = book_soup.find("table")
+
+    # Extract the data from the book page's table
+    n = 0
+    for label_itm in Information_Table.findAll("th"):
+        value_item = Information_Table.select('td')[n].text
+        table_data[label_itm.text] = value_item
+        n += 1
+
+    # Extract the book title
+    book_title = book_soup.find("h1").text
+    table_data['book_title'] = book_title
+
+    # Extract the image URL
+    image_url = book_soup.find("img")["src"]
+    table_data['image_url'] = image_url
+
+    # Extract the product description (assumption: descriptions will be more _
+    # than 60 characters; other targets not usable because aren't as long)
+    for product_description in book_soup.find_all("p"):
+        if len(product_description.text) > 60:
+            product_description = product_description.text
+            table_data['product_description'] = product_description
 
     # Extract the Star Rating
-    # for p in book_soup.find_all("p"):
-    #     for item in p.find_all(class_=''star-rating"):
-    #         if p['class'] == 'star':
-    #             scrape_review_rating = item.select('p.star-rating ')
-    #             table_data['review_rating'] = "scrape_review_rating"
-    # Returns multiple errors: Gotta work on this one a bit. . .
+    for rating in book_soup.find_all("p", class_="star-rating"):
+        ex_class = rating["class"]
+        rating_str = ex_class[1]
+        # print(rating_str)
+        if rating_str == 'One':
+            rating_int = int(rating_str.replace("One", '1'))
+        elif rating_str == 'Two':
+            rating_int = int(rating_str.replace("Two", '2'))
+        elif rating_str == 'Three':
+            rating_int = int(rating_str.replace("Three", '3'))
+        elif rating_str == 'Four':
+            rating_int = int(rating_str.replace("Four", '4'))
+        elif rating_str == 'Five':
+            rating_int = int(rating_str.replace("Five", '5'))
+        else: rating_int = 'whoops'
+    table_data['rating'] = rating_int
 
-    for rating in book_soup.find_all("p"):
-        rating_2 = rating.__class__
-        print(rating_2)
-        # this currently prints class 'bs4.element.Tag' which doesn't seem right.
-
-# print(table_data)  # [this prints out all the data I want. . . why doesn't it print to CSV?]
+print(table_data)  # [this prints out all the data I want. . . why doesn't it print to CSV?]
 # print(table_data.keys())  # this prints out just the fields within the dictionary.
 
 #    #Write it all to a CSV file
@@ -111,13 +116,13 @@ for book in books:
 #         # this just prints the same row on repeat with stupid formatting. . .
 #         # in this version, I tried using DictWriter.
 
-# with open('results.csv', 'w', errors='replace', newline="") as csvFile:
-#     writer = csv.DictWriter(csvFile, delimiter=",", fieldnames=table_data.keys())
-#     writer.writeheader()
-#     for data in table_data:
-#         writer.writerow(table_data)
-#           # this prints the same row on repeat with improved formatting...
-#           # In this version, I changed delimiter to a comma.
+with open('results.csv', 'w', errors='replace', newline="") as csvFile:
+    writer = csv.DictWriter(csvFile, delimiter=",", fieldnames=table_data.keys())
+    writer.writeheader()
+    for data in table_data:
+        writer.writerow(table_data)
+          # this prints the same row on repeat with improved formatting...
+          # In this version, I changed delimiter to a comma.
 
 # with open('results.csv', 'w', errors='replace', newline="") as csvFile:
 #     writer = csv.DictWriter(csvFile, delimiter=",", fieldnames=table_data.keys())
